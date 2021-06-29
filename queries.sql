c2f1f1c7f9604591318b648c73488f8996b9999c
Hitz325 <thitschler88@gmail.com>
2021-06-28 22:54:07 -0500

    Finished through Part 2, still need to Test with SQL #4.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,7 @@
 ## Part 1: Test it with SQL
+SELECT * FROM techjobs.job;
+columns: id, employer, name, skills
+data types: int, string, string, string
 
 ## Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Employer extends AbstractEntity {
+import jdk.jfr.DataAmount;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(min = 3, max = 150, message = "location must be between 3 and 150 characters long")
+    private String location;
+
+//    @ManyToOne
+//    private Employer employer;
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
